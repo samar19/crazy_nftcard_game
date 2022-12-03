13fbd580c327176747529c3a95c695ba3cd3878d
samar abdelhmeed <samar.abdelhmeed77@gmail.com>
2022-12-03 02:20:46 +0200

    add client side

---
+++ client/src/page/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-5xl p-3">Crazy Card </h1>
+      <h2 className="text-3xl p-3">Web3 NFT Card Game</h2>
+      <p className="text-xl p-3">Made with by Ms-Hack</p>
+    </div>
+  )
+};
+
+export default Home;
